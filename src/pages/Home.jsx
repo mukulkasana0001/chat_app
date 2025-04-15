c27fefa8b8c6ca1092c1a92509158abf4e538e83
mukul kasana <mukulkasana0001@gmail.com>
2025-04-15 11:35:20 +0530

    url added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+const socket = io("https://server-repo-phi.vercel.app/");
 
 function App() {
 
